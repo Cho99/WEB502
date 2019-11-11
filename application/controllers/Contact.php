a1642f8e32e6e757a64b21c644d9fc06f91dac45
quang anh <gitcho999@gmail.com>
2019-11-11 08:17:20 +0700

    Send Email

--- application/controllers/Contact.php
+++ application/controllers/Contact.php
@@ -5,10 +5,56 @@ class Contact extends MY_Controller {
 	public function __construct()
 	{
 		parent::__construct();
+		$this->load->model('Contact_model');
+		$this->load->library('email');
 	}
 
 	public function index()
 	{
+		$list = $this->Contact_model->get_list();
+		$this->data['list'] = $list;
+		if ($this->input->post()) {
+
+			$email    = $this->input->post('email');
+			$subject  = $this->input->post('tieude');
+			$ten      = $this->input->post('hoten');
+			$sdt      = $this->input->post('sdt');
+			$tt       = $this->input->post('tt');
+			$sokhach  = $this->input->post('sokhach');	
+			$diachi   = $this->input->post('diachi');
+			$noidung  = $this->input->post('noidung');
+
+			if ($tt == "1") {
+				$tt = "Du lịch";
+			}else {
+				$tt = "Chăm sóc khách hàng";
+			}
+
+			$config['protocol'] 	= 'smtp';
+			$config['smtp_host'] 	= 'ssl://smtp.googlemail.com'; //neu sử dụng gmail
+			$config['smtp_user'] 	= 'tombeo2456@gmail.com';
+			$config['smtp_pass'] 	= 'ovhezcjqhlgbosne';
+			$config['smtp_port'] 	= '465'; //nếu sử dụng gmail
+			$config['charset']  = 'utf-8';
+			$config['newline']  = "\r\n";
+
+			$this->email->initialize($config);
+			$this->email->from('admin_Dog@gmail.com', '502-Travel.com.vn');
+			$this->email->to($email);
+			$this->email->subject($subject);
+
+			$message = $tt . "\n\n" . 'Họ và tên: ' . $ten . "\n\n" . "Số điện thoại: " . $sdt ."\n\n". "Địa chỉ: ". $diachi . "\n\n" ."Nội dung: " . "\n\n" . $noidung;
+
+			$this->email->message($message);
+			if ($this->email->send()) {
+				$this->session->set_flashdata('success', 'Thành cồn');
+				$this->data['temp'] = 'site/contact/index';
+				$this->load->view('site/layout', $this->data);
+			} else {
+
+			}
+		}
+
 		$this->data['temp'] = 'site/contact/index';
 		$this->load->view('site/layout', $this->data);
 	}
--- application/views/site/contact/index.php
+++ application/views/site/contact/index.php
@@ -27,14 +27,18 @@
 					</div>
 				</div>
 				<div class="col-xs-12 lhmarginn">
-					<form class="send_lienhe">
+					<form class="send_lienhe" method="post">
 						<div class="frame-contact">
 							<div class="row">
+								<div class="col-xs-12 mg-bot15">
+									<label>Tiêu đề (<span class="star">*</span>)</label>
+									<input type="text" class="form-control" required="required" name="tieude">
+								</div>
 								<div class="col-lg-4">
 									<label>Loại thông tin (" 
 										<span class="star">*</span>")
 									</label>
-									<select class="form-control">
+									<select class="form-control" name="tt">
 										<option value="1">Du lịch</option>
 										<option value="2">Chăm sóc khách hàng</option>
 									</select>
@@ -43,165 +47,74 @@
 									<label>Họ tên (" 
 										<span class="star">*</span>")
 									</label>
-									<input type="text" class="form-control" required="required" name="txtHoTen" id="txtHoTen">
+									<input type="text" class="form-control" required="required" name="hoten" id="txtHoTen">
 								</div>
 								<div class="col-lg-4">
 									<label>Email (" 
 										<span class="star">*</span>")
 									</label>
-									<input type="email" class="form-control" required="required" name="txtEmail" id="txtEmail">
+									<input type="email" class="form-control" required="required" name="email" id="txtEmail">
 								</div>
 								<div class="col-lg-4">
 									<label>Điện thoại (" 
 										<span class="star">*</span>")
 									</label>
-									<input type="text" class="form-control" required="required" name="txtDienThoai" id="txtDienThoai">
-								</div>
-								<div class="col-lg-4">
-									<label>Tên công ty</label>
-									<input type="text" class="form-control" name="txtTenCongTy">
+									<input type="text" class="form-control" required="required" name="sdt" id="txtDienThoai">
 								</div>
 								<div class="col-lg-4">
 									<label>Số khách</label>
-									<input type="text" class="form-control" name="txtSoKhach">
+									<input type="text" class="form-control" name="sokhach">
 								</div>
 								<div class="col-xs-12 mg-bot15">
 									<label>Địa chỉ</label>
-									<input type="text" class="form-control" name="txtDiaChi">
+									<input type="text" class="form-control" name="diachi">
 								</div>
-								<div class="col-xs-12 mg-bot15">
-									<label>Tiêu đề (<span class="star">*</span>)</label>
-									<input type="text" class="form-control" required="required" name="txtTieuDe">
-								</div>
+								
 								<div class="col-xs-12 mg-bot30">
 									<label>Nội dung (<span class="star">*</span>)</label>
-									<textarea class="form-control" rows="4" cols="5" required="required" name="txtNoiDung"></textarea>
+									<textarea class="form-control" rows="4" cols="5" required="required" name="noidung"></textarea>
 								</div>
-	<!-- 								<div class="col-xs-12 mg-bot15">
-										<input id="responseCaptcha" name="responseCaptcha" type="hidden" value="">
-										<div class="g-recaptcha" data-theme="light" data-sitekey="6LcezicTAAAAAIbBsNL6IS14WAowVVhd_iL0hVNf">
-											<div style="width: 304px; height: 78px;">
-												<div>
-													<iframe src="https://www.google.com/recaptcha/api2/anchor?ar=1&amp;k=6LcezicTAAAAAIbBsNL6IS14WAowVVhd_iL0hVNf&amp;co=aHR0cHM6Ly90cmF2ZWwuY29tLnZuOjQ0Mw..&amp;hl=vi&amp;v=Zy-zVXWdnDW6AUZkKlojAKGe&amp;theme=light&amp;size=normal&amp;cb=w2p3wfi8ilyc" width="304" height="78" role="presentation" name="a-d0poy7wyqnbv" frameborder="0" scrolling="no" sandbox="allow-forms allow-popups allow-same-origin allow-scripts allow-top-navigation allow-modals allow-popups-to-escape-sandbox"></iframe>
-												</div>
-												<textarea id="g-recaptcha-response" name="g-recaptcha-response" class="g-recaptcha-response" style="width: 250px; height: 40px; border: 1px solid rgb(193, 193, 193); margin: 10px 25px; padding: 0px; resize: none; display: none;"></textarea>
-											</div>
-										</div>
-									</div> -->
-									<div class="col-xs-12 text-center gui_lh">
-										<button type="submit" class="btn btn-md btn-general">Gửi đi &nbsp;&nbsp;<i class="fas fa-paper-plane"></i></button>
-									</div>
 
+								<div class="col-xs-12 text-center gui_lh">
+									<button type="submit" class="btn btn-md btn-general">Gửi đi &nbsp;&nbsp;<i class="fas fa-paper-plane"></i></button>
 								</div>
-							</div>
-						</form>
-					</div>
-					<div class="col-xs-12 chinhanh">
-						<div class="row">
-							<div class="col-lg-4 col-md-4 col-sm-6 col-xs-12 mg-bot30">
-								<div class="frame-cn">
-									<div class="tencn">Hồ Chí Minh</div>
-									<div class="mg-bot7">
-										<div class="i-con"><i class="fas fa-map-signs"></i></div>
-										<div class="i-text">190 Pasteur, Quận 3, Tp. Hồ Chí Minh, Việt Nam</div>
-										<div class="clear"></div>
-									</div>
-									<div class="mg-bot7">
-										<div class="i-con"><i class="fas fa-phone"></i></div>
-										<div class="i-text">Tel: (84-28) 3822 8898</div>
-										<div class="clear"></div>
-									</div>
-									<div class="mg-bot7">
-										<div class="i-con"><i class="fas fa-fax"></i></div>
-										<div class="i-text">Fax: (84-28) 3829 9142</div>
-										<div class="clear"></div>
-									</div>
-									<div class="mg-bot7">
-										<div class="i-con"><i class="fas fa-envelope"></i></div>
-										<div class="i-text">Email: info@vietravel.com</div>
-										<div class="clear"></div>
-									</div>
-								</div>
-							</div>
 
-							<div class="col-lg-4 col-md-4 col-sm-6 col-xs-12 mg-bot30">
-								<div class="frame-cn">
-									<div class="tencn">Chi Nhánh Hà Nội</div>
-									<div class="mg-bot7">
-										<div class="i-con"><i class="fas fa-map-signs"></i></div>
-										<div class="i-text">03 Hai Bà Trưng, Quận Hoàn Kiếm, Hà Nội</div>
-										<div class="clear"></div>
-									</div>
-									<div class="mg-bot7">
-										<div class="i-con"><i class="fas fa-phone"></i></div>
-										<div class="i-text">Tel: (84-24) 3933 1978</div>
-										<div class="clear"></div>
-									</div>
-									<div class="mg-bot7">
-										<div class="i-con"><i class="fas fa-fax"></i></div>
-										<div class="i-text">Fax: (84-24) 3933 1979</div>
-										<div class="clear"></div>
-									</div>
-									<div class="mg-bot7">
-										<div class="i-con"><i class="fas fa-envelope"></i></div>
-										<div class="i-text">Email: vtv.hanoi@vietravel.com</div>
-										<div class="clear"></div>
-									</div>
-								</div>
-							</div>
-							<div class="col-lg-4 col-md-4 col-sm-6 col-xs-12 mg-bot30">
-								<div class="frame-cn">
-									<div class="tencn">Chi Nhánh Đà Nẵng</div>
-									<div class="mg-bot7">
-										<div class="i-con"><i class="fas fa-map-signs"></i></div>
-										<div class="i-text">58 Pasteur, Hải Châu, Đà Nẵng</div>
-										<div class="clear"></div>
-									</div>
-									<div class="mg-bot7">
-										<div class="i-con"><i class="fas fa-phone"></i></div>
-										<div class="i-text">Tel: (84-236) 3863 544</div>
-										<div class="clear"></div>
-									</div>
-									<div class="mg-bot7">
-										<div class="i-con"><i class="fas fa-fax"></i></div>
-										<div class="i-text">Fax: (84-236) 3863 571</div>
-										<div class="clear"></div>
-									</div>
-									<div class="mg-bot7">
-										<div class="i-con"><i class="fas fa-envelope"></i></div>
-										<div class="i-text">Email: vtv.danang@vietravel.com</div>
-										<div class="clear"></div>
-									</div>
-								</div>
-							</div>
-							<div class="col-lg-4 col-md-4 col-sm-6 col-xs-12 mg-bot30">
-								<div class="frame-cn">
-									<div class="tencn">Chi Nhánh Cần Thơ</div>
-									<div class="mg-bot7">
-										<div class="i-con"><i class="fas fa-map-signs"></i></div>
-										<div class="i-text">5 Trần Văn Khéo, Cái Khế, Ninh Kiều, Cần Thơ</div>
-										<div class="clear"></div>
-									</div>
-									<div class="mg-bot7">
-										<div class="i-con"><i class="fas fa-phone"></i></div>
-										<div class="i-text">Tel: (84-292) 3763 085</div>
-										<div class="clear"></div>
-									</div>
-									<div class="mg-bot7">
-										<div class="i-con"><i class="fas fa-fax"></i></div>
-										<div class="i-text">Fax: (84-292) 3763 086</div>
-										<div class="clear"></div>
-									</div>
-									<div class="mg-bot7">
-										<div class="i-con"><i class="fas fa-envelope"></i></div>
-										<div class="i-text">Email: vtv.cantho@vietravel.com</div>
-										<div class="clear"></div>
-									</div>
-								</div>
 							</div>
 						</div>
+					</form>
+				</div>
+				<div class="col-xs-12 chinhanh">
+					<div class="row">
+						<?php foreach ($list as $row): ?>
+							<div class="col-lg-4 col-md-4 col-sm-6 col-xs-12 mg-bot30">
+								<div class="frame-cn">
+									<div class="tencn"><?= $row->ten ?></div>
+									<div class="mg-bot7">
+										<div class="i-con"><i class="fas fa-map-signs"></i></div>
+										<div class="i-text"><?= $row->diachi ?></div>
+										<div class="clear"></div>
+									</div>
+									<div class="mg-bot7">
+										<div class="i-con"><i class="fas fa-phone"></i></div>
+										<div class="i-text">Tel: (84-28) <?= $row->sdt ?></div>
+										<div class="clear"></div>
+									</div>
+									<div class="mg-bot7">
+										<div class="i-con"><i class="fas fa-fax"></i></div>
+										<div class="i-text">Fax: (84-28) <?= $row->fax ?></div>
+										<div class="clear"></div>
+									</div>
+									<div class="mg-bot7">
+										<div class="i-con"><i class="fas fa-envelope"></i></div>
+										<div class="i-text">Email: <?= $row->email ?></div>
+										<div class="clear"></div>
+									</div>
+								</div>
+							</div>
+						<?php endforeach ?>
 					</div>
 				</div>
 			</div>
 		</div>
 	</div>
+</div>
